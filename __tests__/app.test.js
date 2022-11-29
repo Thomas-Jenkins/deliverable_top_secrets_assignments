113a22be5efd027339f4171e33b6dae24ebe6339
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-11-29 15:25:36 -0800

    added signout route to users.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,20 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserServices');
 
 const mockUser = {
   email: 'test@test.com',
   password: '12345',
+};
+
+const signUpAndLogin = async (userProps = {}) => {
+  const password = userProps ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser,  ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('users test routes', () => {
@@ -14,13 +24,19 @@ describe('users test routes', () => {
   });
 
   it('creates a new user', async  () => {
-    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request(app).post('/api/v1/topSecret/users').send(mockUser);
     const { email } = mockUser;
     
     expect(res.body).toEqual({
       id: expect.any(String),
       email,
     });
+  });
+
+  it('checks if a user can sign up and log in', async () => {
+    const [agent] = await signUpAndLogin();
+    const temp = await agent.post('/api/v1/topSecret/users/sessions').send(mockUser);
+    expect(temp.body.message).toEqual('Successfully Signed In');
   });
 
   afterAll(() => {
--- sql/setup.sql
+++ sql/setup.sql
@@ -2,7 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP TABLE IF EXISTS topSecret;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users cascade;
 
 CREATE TABLE topSecret (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
