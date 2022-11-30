ce4b384307cb5ddd440de4d81d93aea2a85caecc
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-11-30 09:57:13 -0800

    wrote passing test to check if ONLY logged in users can read secrets route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,7 +12,7 @@ const mockUser = {
 const signUpAndLogin = async (userProps = {}) => {
   const password = userProps ?? mockUser.password;
   const agent = request.agent(app);
-  const user = await UserService.create({ ...mockUser,  ...userProps });
+  const user = await UserService.create({ ...mockUser, ...userProps });
   const { email } = user;
   await agent.post('/api/v1/users/sessions').send({ email, password });
   return [agent, user];
@@ -23,10 +23,12 @@ describe('users test routes', () => {
     return setup(pool);
   });
 
-  it('POST creates a new user', async  () => {
-    const res = await request(app).post('/api/v1/topSecret/users').send(mockUser);
+  it('POST creates a new user', async () => {
+    const res = await request(app)
+      .post('/api/v1/topSecret/users')
+      .send(mockUser);
     const { email } = mockUser;
-    
+
     expect(res.body).toEqual({
       id: expect.any(String),
       email,
@@ -35,17 +37,52 @@ describe('users test routes', () => {
 
   it('POST checks if a user can sign up and log in', async () => {
     const [agent] = await signUpAndLogin();
-    const temp = await agent.post('/api/v1/topSecret/users/sessions').send(mockUser);
+    const temp = await agent
+      .post('/api/v1/topSecret/users/sessions')
+      .send(mockUser);
     expect(temp.body.message).toEqual('Successfully Signed In');
   });
 
   it('DELETE tests if a user can log out', async () => {
     const [agent] = await signUpAndLogin();
-    const resp = await agent.post('/api/v1/topSecret/users/sessions').send(mockUser);
+    const resp = await agent
+      .post('/api/v1/topSecret/users/sessions')
+      .send(mockUser);
     expect(resp.body.message).toEqual('Successfully Signed In');
 
     const respTwo = await agent.delete('/api/v1/topSecret/users/sessions');
-    expect(respTwo.body.message).toEqual('You have signed out')
+    expect(respTwo.body.message).toEqual('You have signed out');
+  });
+
+  it.only('GET tests if ONLY logged in users can view the secrets', async () => {
+    const resp = await request(app).get('/api/v1/topSecret/secrets/');
+    expect(resp.status).toBe(401);
+    expect(resp.body.message).toEqual(
+      'Please sign in before attempting to continue. Your attempt has been reported to the Administrator'
+    );
+    const [agent] = await signUpAndLogin();
+    const temp = await agent
+      .post('/api/v1/topSecret/users/sessions')
+      .send(mockUser);
+    expect(temp.body.message).toEqual('Successfully Signed In');
+    const respTwo = await agent.get('/api/v1/topSecret/secrets/');
+    expect(respTwo.status).toBe(200);
+    expect(respTwo.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "created_at": "2022-11-30T17:56:05.945Z",
+          "description": "Declared when the cafeteria has run out of salsbury steak",
+          "id": "1",
+          "title": "The Chair is Against the Wall",
+        },
+        Object {
+          "created_at": "2022-11-30T17:56:05.945Z",
+          "description": "After the year 2019 all birds have been replaced by government survellience drones",
+          "id": "2",
+          "title": "Birds are not real",
+        },
+      ]
+    `);
   });
 
   afterAll(() => {
